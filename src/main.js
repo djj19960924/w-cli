1bfbd4bd1ef3ef0f5c566c8cf6e909b2340584d1
daijinjin <daijinjin>
2022-05-28 15:05:06 +0800

    提交1,2,3
    提交2
    
    提交3

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@
 /*
  * @Author: djj
  */
+/*
+ * @Author: djj
+ */
 import Vue from 'vue'
 import App from '@/App'
 import router from '@/router'
-
-
+console.log(1)
+console.log(2)
+console.log(3)
 
 new Vue({
   router,
