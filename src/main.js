37d013cd176b248ab93f72b833e7fafc9ae74ba9
dumping <daijinjin>
2022-07-21 11:14:18 +0800

    ceshi1

--- src/main.js
+++ src/main.js
@@ -10,12 +10,6 @@
 import Vue from 'vue'
 import App from '@/App'
 import router from '@/router'
-
-console.log('git rebase')
-console.log("继续开发，并完成" )
-console.log('git rebase')
-console.log("继续开发，并完成" )
-console.log("继续开发，并完成" )
 new Vue({
   router,
   render: h => h(App)
